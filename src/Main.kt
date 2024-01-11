2c1813b15d9b0dec5377b15c72b5a46b6c18f314
ZeinabAhmed72 <Zinabahmed04@gmail.com>
2024-01-11 16:55:24 +0200

    Update

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print("Hello World")
+    print("Hello World")
+    println("Hello World !!")
+    println("Hello World !!")
+    println("Hello World !!")
+    println("Hello World !!")
+    println("Hello World !!")
+
+
+}
