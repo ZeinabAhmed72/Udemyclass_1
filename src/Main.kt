e6a804f0e5d763d2e66d1408f74130ad323f6c6f
ZeinabAhmed72 <Zinabahmed04@gmail.com>
2024-01-12 16:56:13 +0200

    Update

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,31 @@
+
 fun main() {
-    print("Hello World")
-    print("Hello World")
-    println("Hello World !!")
-    println("Hello World !!")
-    println("Hello World !!")
-    println("Hello World !!")
-    println("Hello World !!")
+    val firstName2:String ="Ahmed"
+//firstName2 = "Ali"
+    println(firstName2.isEmpty())
+
+    val myAge:Int = 30
+//myAge = 66
+    println(myAge)
+
+    println("===============")
 
 
+
+
+    var firstName = "Zeinab"
+    firstName = "Hassan"
+println(firstName.isEmpty())
+
+    var lastName= "Ahmed"
+    println(lastName)
+
+    var age = 28
+    println(age)
+
+    println("===========")
+    var car:String = "BMW"
+    println(car)
+    var number:Int = 34
+    println(number)
 }
